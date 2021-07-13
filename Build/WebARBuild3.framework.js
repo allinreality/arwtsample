9162d89f2cb6a2c6638dd71153eb9b2a2bc36ecf
AllinReality <66438476+allinreality@users.noreply.github.com>
2021-07-13 12:50:58 +0530

    update with UI

--- Build/WebARBuild3.framework.js
+++ Build/WebARBuild3.framework.js
@@ -2395,7 +2395,7 @@ function _emscripten_asm_const_ii(code, a0) {
  return ASM_CONSTS[code](a0);
 }
 STATIC_BASE = GLOBAL_BASE;
-STATICTOP = STATIC_BASE + 2704848;
+STATICTOP = STATIC_BASE + 2705040;
 __ATINIT__.push({
  func: (function() {
   __GLOBAL__sub_I_AccessibilityScriptingClasses_cpp();
@@ -4445,7 +4445,7 @@ __ATINIT__.push({
   ___emscripten_environ_constructor();
  })
 });
-var STATIC_BUMP = 2704848;
+var STATIC_BUMP = 2705040;
 Module["STATIC_BASE"] = STATIC_BASE;
 Module["STATIC_BUMP"] = STATIC_BUMP;
 var tempDoublePtr = STATICTOP;
